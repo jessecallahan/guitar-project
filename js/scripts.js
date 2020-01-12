d7cf9445a8353543b257e665d41dea231722d935
Jesse Callahan <jessetylercallahan@gmail.com>
2020-01-12 11:02:39 -0800

    starts the process

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+
+  $(document).ready(function() {
+    $(".submit").click(function(event) {
+      event.preventDefault();
+
+    //   var GuiTar = $("input#enter").val();
+
+    // if (GuiTar == val "a2"){
+    //     $(".output").append("F#")
+
+    //     else {
+            $(".output").append("I don't know")
+        // }
+
+
+    });
+
+});
